f5dc88b4506594b5c59c7d579aa0c422c04c80f5
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-09-29 08:07:18 -0400

    WIP Add DRAKE_THROW_UNLESS_EQ

--- common/test/drake_throw_test.cc
+++ common/test/drake_throw_test.cc
@@ -5,11 +5,22 @@
 
 #include <gtest/gtest.h>
 
+#include "drake/common/test_utilities/expect_throws_message.h"
+
 namespace {
 
 GTEST_TEST(DrakeThrowTest, BasicTest) {
   EXPECT_NO_THROW(DRAKE_THROW_UNLESS(true));
   EXPECT_THROW(DRAKE_THROW_UNLESS(false), std::runtime_error);
+}
+
+GTEST_TEST(DrakeThrowTest, EqTest) {
+  EXPECT_NO_THROW(DRAKE_THROW_UNLESS_EQ(1.0, 1.0));
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      { DRAKE_THROW_UNLESS_EQ(1.0, 2.0); },
+      std::runtime_error,
+      "Failure.*drake_throw_test.*: condition '1.0 == 2.0' failed. "
+      "The operands were '1.000000' and '2.000000'.");
 }
 
 }  // namespace
