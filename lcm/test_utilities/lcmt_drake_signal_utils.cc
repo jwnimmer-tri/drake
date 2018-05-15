49fb861ca801384d61c4151f74821ea35fbdc719
Jeremy Nimmer <jeremy.nimmer@tri.global>
2018-05-15 09:35:22 -0400

    Create test_utilities for lcm

--- lcm/test_utilities/lcmt_drake_signal_utils.cc
+++ lcm/test_utilities/lcmt_drake_signal_utils.cc
@@ -1,4 +1,4 @@
-#include "drake/lcm/lcmt_drake_signal_utils.h"
+#include "drake/lcm/test_utilities/lcmt_drake_signal_utils.h"
 
 #include "drake/common/drake_assert.h"
 #include "drake/common/text_logging.h"
