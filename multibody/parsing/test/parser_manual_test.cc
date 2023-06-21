e45077860b48a6cebce9b30a11e347b5b7bce21b
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-06-20 20:18:27 -0700

    support multiple files

--- multibody/parsing/test/parser_manual_test.cc
+++ multibody/parsing/test/parser_manual_test.cc
@@ -40,7 +40,7 @@ class LevelMonitor final
 };
 
 int do_main(int argc, char* argv[]) {
-  gflags::SetUsageMessage("[INPUT-FILE-OR-URL]\n"
+  gflags::SetUsageMessage("[INPUT-FILE-OR-URL ...]\n"
                           "Run multibody parser; print errors if any");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   if (argc < 2) {
@@ -52,16 +52,6 @@ int do_main(int argc, char* argv[]) {
   // This defeats the `-spdlog_pattern` command line option; oh well.
   drake::logging::set_log_pattern("%v");
 
-  MultibodyPlant<double> plant{0.0};
-  drake::geometry::SceneGraph<double> scene_graph;
-  if (FLAGS_scene_graph) {
-    plant.RegisterAsSourceForSceneGraph(&scene_graph);
-  }
-  Parser parser{&plant};
-  parser.package_map().PopulateFromRosPackagePath();
-  if (FLAGS_strict) {
-    parser.SetStrictParsing();
-  }
   std::shared_ptr<LevelMonitor> level_monitor;
   if (FLAGS_werror) {
     // Add a spdlog sink that detects warnings. Not all warnings come from the
@@ -71,33 +61,53 @@ int do_main(int argc, char* argv[]) {
         .add_sink(level_monitor);
   }
 
-  const bool is_url = std::regex_search(
-      argv[1], std::regex("^[A-Za-z0-9+.-]+://"));
 
-  drake::log()->info("parsing {}", argv[1]);
+  int total_num_models = 0;
+  for (int i = 1; i < argc; ++i) {
+    const std::string file_or_url = argv[i];
+    drake::log()->info("parsing {}", file_or_url);
 
-  // Chances are good that parsing may end in an error (implemented as throw).
-  // For purposes of this tool, we should make its message usable. To do that,
-  // catch it and display it via logging. Yes, this violates the coding
-  // standard; in this case it makes the tool significantly more useful.
-  std::vector<ModelInstanceIndex> models;
-  try {
-    if (is_url) {
-      models = parser.AddModelsFromUrl(argv[1]);
-    } else {
-      models = parser.AddModels(argv[1]);
+    const bool is_url = std::regex_search(
+        file_or_url, std::regex("^[A-Za-z0-9+.-]+://"));
+    MultibodyPlant<double> plant{0.0};
+    drake::geometry::SceneGraph<double> scene_graph;
+    if (FLAGS_scene_graph) {
+      plant.RegisterAsSourceForSceneGraph(&scene_graph);
     }
-  } catch (const std::exception& e) {
-    drake::log()->error(e.what());
-    ::exit(EXIT_FAILURE);
+    Parser parser{&plant};
+    parser.package_map().PopulateFromRosPackagePath();
+    if (FLAGS_strict) {
+      parser.SetStrictParsing();
+    }
+
+    // Chances are good that parsing may end in an error (implemented as throw).
+    // For purposes of this tool, we should make its message usable. To do that,
+    // catch it and display it via logging. Yes, this violates the coding
+    // standard; in this case it makes the tool significantly more useful.
+    std::vector<ModelInstanceIndex> models;
+    try {
+      if (is_url) {
+        models = parser.AddModelsFromUrl(file_or_url);
+      } else {
+        models = parser.AddModels(file_or_url);
+      }
+    } catch (const std::exception& e) {
+      drake::log()->error(e.what());
+      ::exit(EXIT_FAILURE);
+    }
+    if (level_monitor && level_monitor->max_level() >= spdlog::level::warn) {
+      drake::log()->error("Error: Warnings were detected");
+      ::exit(EXIT_FAILURE);
+    }
+    total_num_models += models.size();
   }
-  if (level_monitor && level_monitor->max_level() >= spdlog::level::warn) {
-    drake::log()->error("Error: Warnings were detected");
+  if (total_num_models == 0) {
+    drake::log()->error("Error: No models were parsed");
     ::exit(EXIT_FAILURE);
   }
 
-  drake::log()->info("Parsed {} models.", models.size());
-  return models.empty();
+  drake::log()->info("Parsed {} models.", total_num_models);
+  return 0;
 }
 
 }  // namespace
