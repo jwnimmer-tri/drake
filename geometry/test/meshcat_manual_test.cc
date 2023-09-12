1d161c538d7170a3a070b395045cbd37ee428bf6
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-09-12 12:40:38 -0700

    WIP manual_test debug affordances

--- geometry/test/meshcat_manual_test.cc
+++ geometry/test/meshcat_manual_test.cc
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <thread>
 
+#include <gflags/gflags.h>
+
 #include "drake/common/find_resource.h"
 #include "drake/common/find_runfiles.h"
 #include "drake/common/temp_directory.h"
@@ -20,6 +22,11 @@
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/multibody/tree/prismatic_joint.h"
 #include "drake/systems/analysis/simulator.h"
+#include "drake/systems/sensors/image_writer.h"
+
+DEFINE_int32(image_width, 100, "");
+DEFINE_int32(image_height, 100, "");
+DEFINE_double(image_timeout, 1.0, "");
 
 /* To test, you must manually run `bazel run //geometry:meshcat_manual_test`,
 then follow the instructions on your console. */
@@ -199,318 +206,15 @@ Open up your browser to the URL above.
 )""";
   MaybePauseForUser();
 
-  meshcat->CaptureImage(100, 100);
+  std::cout << "Capturing..." << std::endl;
+  auto image = meshcat->CaptureImage(FLAGS_image_width, FLAGS_image_height, FLAGS_image_timeout);
+  std::cout << "Finished; saving..." << std::endl;
+  SaveToPng(image, "/home/jwnimmer/Downloads/image.png");
+  std::cout << "Saved." << std::endl;
 
   std::cout << "Calling meshcat.Flush(), which will block until all clients "
                "have received all the data)...";
   meshcat->Flush();
-  std::cout << "Done." << std::endl;
-
-  std::cout << "\nAnimations:\n";
-  meshcat->SetCameraPose(animation_target + Vector3d{0, -3, 1.5},
-                         animation_target);
-  std::cout << "The camera has moved to focus on the following animated "
-               "geometries:\n";
-  MeshcatAnimation animation;
-  std::cout << "- the red sphere should move up and down in z.\n";
-  animation.SetTransform(0, "sphere", RigidTransformd(sphere_home));
-  animation.SetTransform(20, "sphere", RigidTransformd(sphere_home +
-                                                       Vector3d::UnitZ()));
-  animation.SetTransform(40, "sphere", RigidTransformd(sphere_home));
-
-  std::cout << "- the blue box should spin clockwise about the +z axis.\n";
-  animation.SetTransform(0, "box",
-                         RigidTransformd(RotationMatrixd::MakeZRotation(0),
-                                         box_home));
-  animation.SetTransform(20, "box",
-                         RigidTransformd(RotationMatrixd::MakeZRotation(M_PI),
-                                         box_home));
-  animation.SetTransform(
-      40, "box", RigidTransformd(RotationMatrixd::MakeZRotation(2 * M_PI),
-                                 box_home));
-  animation.set_repetitions(4);
-
-  std::cout << "- the green cylinder should appear and disappear.\n";
-  animation.SetProperty(0, "cylinder", "visible", true);
-  animation.SetProperty(20, "cylinder", "visible", false);
-  animation.SetProperty(40, "cylinder", "visible", true);
-  animation.set_repetitions(4);
-
-  std::cout
-      << "- the pink ellipsoid should get less and then more transparent.\n";
-  animation.SetProperty(0, "ellipsoid/<object>", "material.opacity", 1.0);
-  animation.SetProperty(20, "ellipsoid/<object>", "material.opacity", 0.0);
-  animation.SetProperty(40, "ellipsoid/<object>", "material.opacity", 1.0);
-  animation.set_repetitions(4);
-
-  meshcat->SetAnimation(animation);
-
-  std::cout << "You can review/replay the animation from the controls menu.\n";
-  MaybePauseForUser();
-
-  meshcat->Set2dRenderMode(math::RigidTransform(Vector3d{0, -3, 0}), -4,
-                           4, -2, 2);
-
-  std::cout << "- The scene should have switched to 2D rendering mode.\n";
-  MaybePauseForUser();
-
-  meshcat->Set2dRenderMode(
-      math::RigidTransform(math::RotationMatrixd::MakeZRotation(-M_PI / 2.0),
-                           sphere_home),
-      -2, 2, -2, 2);
-  // This call shows that SetCameraTarget() is a no-op for orthographic cameras.
-  // The described view should be unaffected by this absurd target point.
-  meshcat->SetCameraTarget(Vector3d{0, -100, -50});
-
-  std::cout << "- Now 2D rendering along the +x axis (red sphere in front).\n";
-  MaybePauseForUser();
-
-  meshcat->SetCameraPose(Vector3d{2, 2, 2}, Vector3d::Zero());
-  std::cout << "- Now we have an isometric 3/4 view.\n";
-  MaybePauseForUser();
-
-  std::cout << "- The scene should have switched back to 3D.\n";
-  meshcat->ResetRenderMode();
-
-  MaybePauseForUser();
-
-  // Turn off the background (it will appear white).
-  meshcat->SetProperty("/Background", "visible", false);
-
-  meshcat->Delete("box");
-  meshcat->SetProperty("/Lights/AmbientLight/<object>", "intensity", 0.1);
-
-  std::cout
-      << "- The blue box should have disappeared\n"
-      << "- The lights should have dimmed.\n"
-      << "- The background should have been disabled (it will appear white)"
-      << std::endl;
-  MaybePauseForUser();
-
-  meshcat->SetCameraTarget(gltf_pose);
-  meshcat->SetProperty("/Background", "visible", true);
-  meshcat->SetEnvironmentMap(
-      FindResourceOrThrow("drake/geometry/test/env_256_cornell_box.png"));
-
-  std::cout << "- An environment map has been loaded from a png -- the Cornell "
-            << "box.\n"
-            << "  The dented green box should reflect it (the camera has moved "
-               "to focus on the box).\n";
-  MaybePauseForUser();
-
-  meshcat->SetEnvironmentMap(
-      FindResourceOrThrow("drake/geometry/test/env_256_brick_room.jpg"));
-
-  std::cout << "- The Cornell box has been replaced by a room with brick walls "
-            << "loaded from a jpg.\n";
-  MaybePauseForUser();
-
-  meshcat->SetEnvironmentMap("");
-  meshcat->SetCameraTarget(Vector3d::Zero());
-  meshcat->Delete();
-  std::cout << "- Everything should have disappeared." << std::endl;
-
-  MaybePauseForUser();
-
-  meshcat->SetProperty("/Lights/AmbientLight/<object>", "intensity", 0.6);
-
-  {
-    systems::DiagramBuilder<double> builder;
-    auto [plant, scene_graph] =
-        multibody::AddMultibodyPlantSceneGraph(&builder, 0.0);
-
-    multibody::Parser parser(&plant);
-
-    // Add the hydroelastic spheres and joints between them.
-    const std::string hydro_sdf =
-        FindResourceOrThrow("drake/multibody/meshcat/test/hydroelastic.sdf");
-    parser.AddModels(hydro_sdf);
-    const auto& body1 = plant.GetBodyByName("body1");
-    plant.AddJoint<multibody::PrismaticJoint>("body1", plant.world_body(),
-                                              std::nullopt, body1, std::nullopt,
-                                              Vector3d::UnitZ());
-    const auto& body2 = plant.GetBodyByName("body2");
-    plant.AddJoint<multibody::PrismaticJoint>("body2", plant.world_body(),
-                                              std::nullopt, body2, std::nullopt,
-                                              Vector3d::UnitX());
-    plant.Finalize();
-
-    MeshcatVisualizerParams params;
-    params.delete_on_initialization_event = false;
-    auto& visualizer = MeshcatVisualizerd::AddToBuilder(
-        &builder, scene_graph, meshcat, std::move(params));
-
-    multibody::meshcat::ContactVisualizerParams cparams;
-    cparams.newtons_per_meter = 60.0;
-    auto& contact = multibody::meshcat::ContactVisualizerd::AddToBuilder(
-        &builder, plant, meshcat, std::move(cparams));
-
-    auto diagram = builder.Build();
-    auto context = diagram->CreateDefaultContext();
-
-    plant.SetPositions(&plant.GetMyMutableContextFromRoot(context.get()),
-                       Eigen::Vector2d{0.1, 0.3});
-    diagram->ForcedPublish(*context);
-    meshcat->SetCameraPose(Vector3d{0, -1.5, 1}, Vector3d{0.25, 0, 0});
-    std::cout << "- Now you should see three colliding hydroelastic spheres."
-              << std::endl;
-    MaybePauseForUser();
-
-    contact.Delete();
-    visualizer.Delete();
-  }
-
-  {
-    meshcat->SetCameraPose(Vector3d{-1.0, -1.0, 1.5}, Vector3d{0, 0, 0.5});
-    systems::DiagramBuilder<double> builder;
-    auto [plant, scene_graph] =
-        multibody::AddMultibodyPlantSceneGraph(&builder, 0.001);
-    multibody::Parser parser(&plant);
-    parser.AddModels(
-        FindResourceOrThrow("drake/manipulation/models/iiwa_description/urdf/"
-                            "iiwa14_spheres_collision.urdf"));
-    plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base"));
-    parser.AddModels(FindResourceOrThrow(
-        "drake/examples/kuka_iiwa_arm/models/table/"
-        "extra_heavy_duty_table_surface_only_collision.sdf"));
-    const double table_height = 0.7645;
-    plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("link"),
-                     RigidTransformd(Vector3d{0, 0, -table_height - 0.01}));
-    plant.Finalize();
-
-    builder.ExportInput(plant.get_actuation_input_port(), "actuation_input");
-    MeshcatVisualizerParams params;
-    params.delete_on_initialization_event = false;
-    auto& visualizer = MeshcatVisualizerd::AddToBuilder(
-        &builder, scene_graph, meshcat, std::move(params));
-
-    multibody::meshcat::ContactVisualizerParams cparams;
-    cparams.newtons_per_meter = 60.0;
-    multibody::meshcat::ContactVisualizerd::AddToBuilder(
-        &builder, plant, meshcat, std::move(cparams));
-
-    auto diagram = builder.Build();
-    auto context = diagram->CreateDefaultContext();
-    diagram->get_input_port().FixValue(context.get(), Eigen::VectorXd::Zero(7));
-
-    diagram->ForcedPublish(*context);
-    std::cout
-        << "- Now you should see a kuka model (from MultibodyPlant/SceneGraph)"
-        << std::endl;
-
-    MaybePauseForUser();
-
-    std::cout << "Now we'll run the simulation...\n"
-              << "- You should see the robot fall down and hit the table\n"
-              << "- You should see the contact force vectors (when it hits)\n"
-              << std::endl;
-
-    systems::Simulator<double> simulator(*diagram, std::move(context));
-    simulator.set_target_realtime_rate(1.0);
-    visualizer.StartRecording();
-    simulator.AdvanceTo(4.0);
-    visualizer.PublishRecording();
-
-    std::cout
-        << "The recorded simulation results should now be available as an "
-           "animation.  Use the animation GUI to confirm."
-        << std::endl;
-
-    MaybePauseForUser();
-  }
-
-  meshcat->SetEnvironmentMap(
-      FindResourceOrThrow("drake/geometry/test/env_256_cornell_box.png"));
-  meshcat->SetCameraTarget(Vector3d{-0.4, 0, 0});
-  const std::string html_filename(temp_directory() + "/meshcat_static.html");
-  std::ofstream html_file(html_filename);
-  html_file << meshcat->StaticHtml();
-  html_file.close();
-  meshcat->SetEnvironmentMap("");
-  meshcat->SetCameraPose(Vector3d{-1.0, -1.0, 1.5}, Vector3d{0, 0, 0.5});
-
-  std::cout << "A standalone HTML file capturing this scene. In addition the "
-               "standalone file includes:\n"
-               "   - the animation shown here\n"
-               "   - an additional environment map (not shown here)\n"
-               "   - the camera moved to focus on the contact point between "
-               "robot and table\n"
-               "The file has been written to:\n"
-            << "  file://" << html_filename
-            << "\nOpen that location in your browser now and confirm that "
-               "the iiwa is visible, the animation plays, the environment map "
-               "is present, and the camera is positioned as indicated."
-            << std::endl;
-
-  MaybePauseForUser();
-
-  std::remove(html_filename.c_str());
-  std::cout
-      << "Note: I've deleted the temporary HTML file (it's several Mb).\n\n";
-
-  meshcat->AddButton("ButtonTest");
-  meshcat->AddButton("Press t Key");
-  meshcat->AddButton("Press t Key", "KeyT");  // Now the keycode is assigned.
-  meshcat->AddSlider("SliderTest", 0, 1, 0.01, 0.5, "ArrowLeft", "ArrowRight");
-
-  std::cout << "I've added two buttons and a slider to the controls menu.\n";
-  std::cout << "- Click the ButtonTest button a few times.\n";
-  std::cout << "- Press the 't' key in the meshcat window, which "
-               "should be equivalent to pressing the second button.\n";
-  std::cout << "The buttons do nothing, but the total number of clicks for "
-               "each button will be reported after you press RETURN.\n";
-  std::cout << "- Move SliderTest slider.\n";
-  std::cout << "- Confirm that the ArrowLeft and ArrowRight keys also move the "
-               "slider.\n";
-  std::cout << "- Open a second browser (" << meshcat->web_url()
-            << ") and confirm that moving the slider in one updates the slider "
-               "in the other.\n";
-
-  MaybePauseForUser();
-
-  std::cout << "Got " << meshcat->GetButtonClicks("ButtonTest")
-            << " clicks on ButtonTest.\n"
-            << "Got " << meshcat->GetButtonClicks("Press t Key")
-            << " clicks on \"Press t Key\".\n"
-            << "Got " << meshcat->GetSliderValue("SliderTest")
-            << " value for SliderTest.\n\n" << std::endl;
-
-  std::cout << "Next, we'll test gamepad (i.e., joystick) features.\n\n";
-  std::cout
-      << "While the Meshcat browser window has focus, click any button on "
-      << "your gamepad to activate gamepad support in the browser.\n\n";
-  std::cout
-      << "Then(after you press RETURN), we'll print the gamepad stats for 5 "
-      << "seconds. During that time, move the control sticks and hold some "
-      << "buttons and you should see those values reflected in the printouts. "
-      << "As long as you see varying values as you move the controls, that's "
-      << "sufficient to consider the test passing; the exact values do not "
-      << "matter.\n";
-
-  MaybePauseForUser();
-
-  Meshcat::Gamepad gamepad = meshcat->GetGamepad();
-  if (!gamepad.index) {
-    std::cout << "No gamepad activity detected.\n";
-  } else {
-    for (int i = 0; i < 5; ++i) {
-      gamepad = meshcat->GetGamepad();
-      std::cout << "Gamepad status:\n";
-      std::cout << "  gamepad index: " << *gamepad.index << "\n";
-      std::cout << "  buttons: ";
-      for (auto const& value : gamepad.button_values) {
-        std::cout << value << ", ";
-      }
-      std::cout << "\n";
-      std::cout << "  axes: ";
-      for (auto const& value : gamepad.axes) {
-        std::cout << value << ", ";
-      }
-      std::cout << "\n";
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-  }
-
   std::cout << "Exiting..." << std::endl;
   return 0;
 }
@@ -518,4 +222,8 @@ Open up your browser to the URL above.
 }  // namespace geometry
 }  // namespace drake
 
-int main() { return drake::geometry::do_main(); }
+int main(int argc, char** argv) {
+  google::SetUsageMessage(" ");  // Nerf a silly warning emitted by gflags.
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  return drake::geometry::do_main();
+}
