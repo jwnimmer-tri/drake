82bbc66afd90c8e556c78b22ee3bea5773dcc9ea
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-10-10 19:08:10 -0700

    f

--- common/yaml/test/yaml_write_archive_test.cc
+++ common/yaml/test/yaml_write_archive_test.cc
@@ -209,7 +209,7 @@ TEST_F(YamlWriteArchiveTest, Variant) {
   test(Variant4(DoubleStruct{1.0}), "!DoubleStruct\n    value: 1.0");
   test(Variant4(EigenVecStruct{Eigen::Vector2d(1.0, 2.0)}),
        "!EigenStruct\n    value: [1.0, 2.0]");
-  test(Variant4(std::string()), "!!float 1.0");
+  test(Variant4(double{1.0}), "!!float 1.0");
 }
 
 TEST_F(YamlWriteArchiveTest, VariantMonoFirst) {
