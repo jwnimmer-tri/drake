d1a90c4006d52cac0740cf415d294c185f27e09f
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-10-14 13:35:46 -0700

    [planning] Replace DofMask vector-of-bool with a dynamic bitset

---
+++ planning/counted_dynamic_bitset.cc
@@ -0,0 +1,231 @@
+#include "drake/planning/counted_dynamic_bitset.h"
+
+#include <algorithm>
+#include <cstring>
+#include <utility>
+
+namespace drake {
+namespace planning {
+namespace internal {
+
+namespace {
+
+// Our inline storage uses uint64_t.
+constexpr int kMaxInlineSize = 64;
+
+// Returns the number of 1 bits in `x`.
+int popcount(uint64_t x) {
+  return __builtin_popcount(x);
+}
+
+// Copies a span of bools into a new unique_ptr<bool[]>.
+// @pre values.size() > kMaxInlineSize
+__attribute__((noinline, cold)) std::unique_ptr<bool[]> MakeBoolArray(
+    std::span<const bool> values) {
+  DRAKE_ASSERT(values.size() > kMaxInlineSize);
+  if (!(values.size() > kMaxInlineSize)) __builtin_unreachable();
+  // TODO(jwnimmer-tri) Use make_unique_for_overwrite once we have >= C++23.
+  std::unique_ptr<bool[]> result(new bool[values.size()]);
+  std::copy(values.begin(), values.end(), result.get());
+  return result;
+}
+
+// Broadcasts a single bool into a new unique_ptr<bool[]>.
+// @pre size > kMaxInlineSize
+__attribute__((noinline, cold)) std::unique_ptr<bool[]> MakeBoolArray(
+    int size, bool value = false) {
+  DRAKE_ASSERT(size > kMaxInlineSize);
+  if (!(size > kMaxInlineSize)) __builtin_unreachable();
+  // TODO(jwnimmer-tri) Use make_unique_for_overwrite once we have >= C++23.
+  std::unique_ptr<bool[]> result(new bool[size]);
+  std::fill_n(result.get(), size, value);
+  return result;
+}
+
+// Copies the bool array at `other` into its complement (negation).
+// @pre size > kMaxInlineSize
+__attribute__((noinline, cold)) std::unique_ptr<bool[]> MakeComplement(
+    std::span<const bool> other) {
+  DRAKE_ASSERT(other.size() > kMaxInlineSize);
+  if (!(other.size() > kMaxInlineSize)) __builtin_unreachable();
+  // TODO(jwnimmer-tri) Use make_unique_for_overwrite once we have >= C++23.
+  std::unique_ptr<bool[]> result(new bool[other.size()]);
+  for (size_t i = 0; i < other.size(); ++i) {
+    result[i] = !other[i];
+  }
+  return result;
+}
+
+// The template argument for Merge(), immediately below.
+enum BinaryOperator {
+  kUnion,
+  kIntersect,
+  kSubtract,
+};
+
+// Returns a new bool array computed as a binary operator over the equally-
+// sized `a` and `b` input arrays.
+// @pre size > kMaxInlineSize
+template <BinaryOperator operation>
+__attribute__((noinline,
+               cold)) std::pair<std::unique_ptr<bool[]>, int /* count */>
+Merge(int size, const bool* a, const bool* b) {
+  DRAKE_ASSERT(size > kMaxInlineSize);
+  if (!(size > kMaxInlineSize)) __builtin_unreachable();
+  // TODO(jwnimmer-tri) Use make_unique_for_overwrite once we have >= C++23.
+  std::unique_ptr<bool[]> buffer(new bool[size]);
+  int count = 0;
+  for (int i = 0; i < size; ++i) {
+    int bit;
+    if constexpr (operation == kUnion) {
+      bit = int{a[i]} | int{b[i]};
+    } else if constexpr (operation == kIntersect) {
+      bit = int{a[i]} & int{b[i]};
+    } else if constexpr (operation == kSubtract) {
+      bit = int{a[i]} & ~int{b[i]};
+    } else {
+      DRAKE_UNREACHABLE();
+    }
+    buffer[i] = (bit != 0);
+    count += bit;
+  }
+  return std::make_pair(std::move(buffer), count);
+}
+
+}  // namespace
+
+CountedDynamicBitset::CountedDynamicBitset(int size, bool value) {
+  DRAKE_THROW_UNLESS(size >= 0);
+  size_ = size;
+  count_ = value ? size : 0;
+  if (size <= kMaxInlineSize) [[likely]] {
+    if (value) {
+      inline_ = mask();
+    }
+  } else {
+    buffer_ = MakeBoolArray(size, value);
+  }
+}
+
+CountedDynamicBitset::CountedDynamicBitset(std::span<const bool> values) {
+  size_ = ssize(values);
+  if (size_ <= kMaxInlineSize) [[likely]] {
+    for (int i = 0; i < size_; ++i) {
+      const bool bit = values[i];
+      inline_ |= (uint64_t{bit} << i);
+    }
+    count_ = popcount(inline_);
+  } else {
+    buffer_ = MakeBoolArray(values);
+    count_ = std::count(values.begin(), values.end(), true);
+  }
+}
+
+// We can't use the defaulted implementation because of our unique_ptr member.
+// However, for simplicity we can just delegate to the copy-assignment operator.
+CountedDynamicBitset::CountedDynamicBitset(const CountedDynamicBitset& other) {
+  *this = other;
+}
+
+// We can't use the defaulted implementation because of our unique_ptr member.
+CountedDynamicBitset& CountedDynamicBitset::operator=(
+    const CountedDynamicBitset& other) {
+  if (this == &other) [[unlikely]] {
+    return *this;
+  }
+  size_ = other.size();
+  count_ = other.count();
+  inline_ = other.inline_;
+  if (other.is_inline()) [[likely]] {
+    buffer_.reset();
+  } else {
+    buffer_ = MakeBoolArray(std::span<const bool>(other.buffer_.get(), other.size_));
+  }
+  return *this;
+}
+
+CountedDynamicBitset CountedDynamicBitset::Complement() const {
+  if (is_inline()) [[likely]] {
+    return CountedDynamicBitset(
+        /* size = */ size(),
+        /* count = */ size() - count(),
+        /* inline = */ ~inline_ & mask());
+  } else {
+    CountedDynamicBitset result(
+        /* size = */ size(),
+        /* count = */ size() - count(),
+        /* inline = */ 0);
+    result.buffer_ = MakeComplement(std::span<const bool>(buffer_.get(), size()));
+    return result;
+  }
+}
+
+CountedDynamicBitset CountedDynamicBitset::Union(
+    const CountedDynamicBitset& other) const {
+  DRAKE_ASSERT(this->size() == other.size());
+  DRAKE_ASSERT(this->is_inline() == other.is_inline());
+  if (is_inline()) [[likely]] {
+    const uint64_t new_bits = (this->inline_ | other.inline_) & mask();
+    return CountedDynamicBitset(size(),
+                                /* count = */ popcount(new_bits),
+                                /* inline = */ new_bits);
+  } else {
+    CountedDynamicBitset result(size(), /* count = */ 0, /* inline = */ 0);
+    std::tie(result.buffer_, result.count_) =
+        Merge<kUnion>(size(), buffer_.get(), other.buffer_.get());
+    return result;
+  }
+}
+
+CountedDynamicBitset CountedDynamicBitset::Intersect(
+    const CountedDynamicBitset& other) const {
+  DRAKE_ASSERT(this->size() == other.size());
+  DRAKE_ASSERT(this->is_inline() == other.is_inline());
+  if (is_inline()) [[likely]] {
+    const uint64_t new_bits = (this->inline_ & other.inline_) & mask();
+    return CountedDynamicBitset(size(),
+                                /* count = */ popcount(new_bits),
+                                /* inline = */ new_bits);
+  } else {
+    CountedDynamicBitset result(size(), /* count = */ 0, /* inline = */ 0);
+    std::tie(result.buffer_, result.count_) =
+        Merge<kIntersect>(size(), buffer_.get(), other.buffer_.get());
+    return result;
+  }
+}
+
+CountedDynamicBitset CountedDynamicBitset::Subtract(
+    const CountedDynamicBitset& other) const {
+  DRAKE_ASSERT(this->size() == other.size());
+  DRAKE_ASSERT(this->is_inline() == other.is_inline());
+  if (is_inline()) [[likely]] {
+    const uint64_t new_bits = (this->inline_ & ~other.inline_) & mask();
+    return CountedDynamicBitset(size(),
+                                /* count = */ popcount(new_bits),
+                                /* inline = */ new_bits);
+  } else {
+    CountedDynamicBitset result(size(), /* count = */ 0, /* inline = */ 0);
+    std::tie(result.buffer_, result.count_) =
+        Merge<kSubtract>(size(), buffer_.get(), other.buffer_.get());
+    return result;
+  }
+}
+
+bool CountedDynamicBitset::operator==(const CountedDynamicBitset& other) const {
+  if (this->size() != other.size()) {
+    return false;
+  }
+  DRAKE_ASSERT(this->is_inline() == other.is_inline());
+  bool result;
+  if (is_inline()) [[likely]] {
+    result = (this->inline_ == other.inline_);
+  } else {
+    result = (std::memcmp(buffer_.get(), other.buffer_.get(), size()) == 0);
+  }
+  DRAKE_ASSERT((result == false) || (this->count() == other.count()));
+  return result;
+}
+
+}  // namespace internal
+}  // namespace planning
+}  // namespace drake
---
+++ planning/counted_dynamic_bitset.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <span>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/common/drake_throw.h"
+#include "drake/common/reset_after_move.h"
+
+namespace drake {
+namespace planning {
+namespace internal {
+
+/* CountedDynamicBitset represents something like a `std::array<bool, N>` but
+with a few customizations:
+- the container size is determined at construction time instead of compile time;
+- the count of values set to `true` is pre-computed and stored;
+- convenient set-logic operations (complement, union, intersect, subtract). */
+class CountedDynamicBitset {
+ public:
+  DRAKE_DECLARE_COPY_AND_MOVE_AND_ASSIGN(CountedDynamicBitset);
+
+  /* Default constructor. Creates a bitset with size() = count() = 0. */
+  CountedDynamicBitset() = default;
+
+  /* Homogenous constructor. Creates a bitset with the given `size` where all
+  values are set to the given `value`.
+  @throws std::exception unless size >= 0 */
+  CountedDynamicBitset(int size, bool value);
+
+  /* Span constructor. Creates a bitset from a span of bools. */
+  explicit CountedDynamicBitset(std::span<const bool> values);
+
+  /* Returns this bitset's size. */
+  int size() const { return size_; }
+
+  /* Returns this bitset's count of values set to `true`. */
+  int count() const { return count_; }
+
+  /* Compares two bitsets for equality. Note that `other.size()` may differ from
+  `this->size()`; such comparisons will report as not equal. */
+  bool operator==(const CountedDynamicBitset& other) const;
+
+  /* Returns the value at the given `index`.
+  @pre `index` is in the range [0, size()). */
+  bool operator[](int index) const {
+    DRAKE_ASSERT(index >= 0 && index < size_);
+    if (is_inline()) {
+      return (inline_ >> index) & 1;
+    } else {
+      return buffer_[index];
+    }
+  }
+
+  /* Returns a new bitset with `true` values replaced with `false` and vice
+  versa. The size is unchanged. */
+  [[nodiscard]] CountedDynamicBitset Complement() const;
+
+  /* Returns a new bitset with the union of `this` and `other`. The i'th bit is
+  `true` iff that bit in either `this` or `other` is `true`.
+  @pre `size() == other.size()`. */
+  [[nodiscard]] CountedDynamicBitset Union(
+      const CountedDynamicBitset& other) const;
+
+  /* Returns a new bitset with the intersection of `this` and `other`. The i'th
+  bit is `true` iff that bit in both `this` and `other` are `true`.
+  @pre `size() == other.size()`. */
+  [[nodiscard]] CountedDynamicBitset Intersect(
+      const CountedDynamicBitset& other) const;
+
+  /* Returns a new bitset with the set difference of `this` and `other`. The
+  i'th bit is `true` iff that bit is `true` in `this` and `false` in `other`.
+  @pre `size() == other.size()`. */
+  [[nodiscard]] CountedDynamicBitset Subtract(
+      const CountedDynamicBitset& other) const;
+
+ private:
+  CountedDynamicBitset(int size, int count, uint64_t inline_in)
+      : size_(size), count_(count), inline_(inline_in) {
+    DRAKE_ASSERT((inline_ & ~mask()) == 0);
+  }
+
+  /* Returns true if inline_ stores our data or false when buffer_ stores it. */
+  bool is_inline() const { return buffer_ == nullptr; }
+
+  /* Returns the representation of `inline_` if all bits were `true`.
+  @pre is_inline() */
+  uint64_t mask() const { return (uint64_t{1} << size()) - 1; }
+
+  // The meaning of `size_` and `count_` are per the size() and count() API.
+  //
+  // For storing the actual values, we have two options:
+  // - For small bitsets, we use the `inline_` bitmask. The bit at
+  //   `(1 << index)` stores the index'th set value.
+  // - For larger bitsets, the we use the `buffer_` array. The bool at
+  //   `buffer_[index]` stores the index'th set value.
+  //
+  // When `inline_` storage is being used, it's high-order bits beyond `size_`
+  // are guaranteed to be zero, and `buffer_` is guaranteed to be null.
+  //
+  // These member fields are almost "const" -- we have no member functions that
+  // mutate them, other than the two assignment operators.
+  reset_after_move<int> size_{0};
+  reset_after_move<int> count_{0};
+  reset_after_move<uint64_t> inline_{0};
+  std::unique_ptr<bool[]> buffer_{nullptr};
+};
+
+// The move operations are cheap so are defined inline. The copy operations are
+// potentially expensive for larger bitsets, so are defined in the cc file.
+
+inline CountedDynamicBitset::CountedDynamicBitset(
+    CountedDynamicBitset&& other) = default;
+
+inline CountedDynamicBitset& CountedDynamicBitset::operator=(
+    CountedDynamicBitset&& other) = default;
+
+}  // namespace internal
+}  // namespace planning
+}  // namespace drake
